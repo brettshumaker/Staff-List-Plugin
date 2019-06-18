217372277cb64aa5ddae979efdbc23ac43ed2d49
brettshumaker <brettshumaker@gmail.com>
2019-06-18 06:43:01 -0500

    Cleaned up defaults
    - Use `[staff-photo]` instead of the hard-coded `img` tag
    - Fixed bug with spacing between Staff Members
    - Removed indenting

--- trunk/includes/class-simple-staff-list-activator.php
+++ trunk/includes/class-simple-staff-list-activator.php
@@ -32,74 +32,74 @@ class Simple_Staff_List_Activator {
 	 */
 	public static function activate( $is_forced = false ) {
 		$default_template = '
-		[staff_loop]
-			<img class="staff-member-photo" src="[staff-photo-url]" alt="[staff-name] : [staff-position]">
-			<div class="staff-member-info-wrap">
-				[staff-name-formatted]
-				[staff-position-formatted]
-				[staff-bio-formatted]
-				[staff-email-link]
-			</div>
-		[/staff_loop]';
+[staff_loop]
+    [staff-photo]
+    <div class="staff-member-info-wrap">
+        [staff-name-formatted]
+        [staff-position-formatted]
+        [staff-bio-formatted]
+        [staff-email-link]
+    </div>
+[/staff_loop]';
 
 		$default_css = '
-			/*  div wrapped around entire staff list  */
-			div.staff-member-listing {
-			}
-			/*  div wrapped around each staff member  */
-			div.staff-member {
-				padding-bottom: 2em;
-				border-bottom: thin dotted #aaa;
-			}
-			/*  "Even" staff member  */
-			div.staff-member.even {
-			}
-			/*  "Odd" staff member  */
-			div.staff-member.odd {
-				margin-top: 2em;
-			}
-			/*  Last staff member  */
-			div.staff-member.last {
-				padding-bottom: 0;
-				border: none;
-			}
-			/*  Wrap around staff info  */
-			.staff-member-info-wrap {
-				float: left;
-				width: 70%;
-				margin-left: 3%;
-			}
-			/*  [staff-bio-formatted]  */
-			div.staff-member-bio {
-			}
-			/*  p tags within [staff-bio-formatted]  */
-			div.staff-member-bio p {
-			}
-			/*  [staff-photo]  */
-			img.staff-member-photo {
-				float: left;
-			}
-			/*  [staff-email-link]  */
-			.staff-member-email {
-			}
-			/*  [staff-name-formatted]  */
-			div.staff-member-listing h3.staff-member-name {
-				margin: 0;
-			}
-			/*  [staff-position-formatted]  */
-			div.staff-member-listing h4.staff-member-position {
-				margin: 0;
-				font-style: italic;
-			}
-			/* Clearfix for div.staff-member */
-			div.staff-member:after {
-				content: "";
-				display: block;
-				clear: both;
-			}
-			/* Clearfix for <= IE7 */
-			* html div.staff-member { height: 1%; }
-			div.staff-member { display: block; }
+/*  div wrapped around entire staff list  */
+div.staff-member-listing {
+}
+/*  div wrapped around each staff member  */
+div.staff-member {
+    padding-bottom: 2em;
+    border-bottom: thin dotted #aaa;
+    margin-bottom: 2em;
+}
+/*  "Even" staff member  */
+div.staff-member.even {
+}
+/*  "Odd" staff member  */
+div.staff-member.odd {
+}
+/*  Last staff member  */
+div.staff-member.last {
+    padding-bottom: 0;
+    border: none;
+}
+/*  Wrap around staff info  */
+.staff-member-info-wrap {
+    float: left;
+    width: 70%;
+    margin-left: 3%;
+}
+/*  [staff-bio-formatted]  */
+div.staff-member-bio {
+}
+/*  p tags within [staff-bio-formatted]  */
+div.staff-member-bio p {
+}
+/*  [staff-photo]  */
+img.staff-member-photo {
+    float: left;
+}
+/*  [staff-email-link]  */
+.staff-member-email {
+}
+/*  [staff-name-formatted]  */
+div.staff-member-listing h3.staff-member-name {
+    margin: 0;
+}
+/*  [staff-position-formatted]  */
+div.staff-member-listing h4.staff-member-position {
+    margin: 0;
+    font-style: italic;
+}
+/* Clearfix for div.staff-member */
+div.staff-member:after {
+    content: "";
+    display: block;
+    clear: both;
+}
+/* Clearfix for <= IE7 */
+* html div.staff-member { height: 1%; }
+div.staff-member { display: block; }
 		';
 
 		$default_tags       = array(
